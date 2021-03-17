2dd019e324b0cbd8e3785b95245fdbf70b6e929f
hackuter J <yashoovardhanjha@gmail.com>
2021-03-18 02:18:29 +0530

    initial commit

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(
             'Content-Type', 
             
             'Origin', 
+            'Access-Control-Allow-Origin'
            
         ], 
         preflightContinue: false 
@@ -33,6 +34,21 @@ app.use(
     bodyparser.json()
     
 );
+
+
+app.all('*', function(req, res, next) {
+
+    res.header('Access-Control-Allow-Origin', '*');
+  
+    res.header('Access-Control-Allow-Methods', 'DELETE, HEAD, GET, OPTIONS, POST, PUT');
+  
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+    next();
+  
+  });
+  
+
 app.listen(process.env.PORT || 4300, () => {
   //console.log("The server started on port 4300 !!!!!!");
 });
