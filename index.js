51e957756ce4676e32f3476351038bc2865384b6
hackuter J <yashoovardhanjha@gmail.com>
2021-03-18 02:19:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
 
 app.all('*', function(req, res, next) {
 
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', "https://students.iiserb.ac.in/");
   
     res.header('Access-Control-Allow-Methods', 'DELETE, HEAD, GET, OPTIONS, POST, PUT');
   
