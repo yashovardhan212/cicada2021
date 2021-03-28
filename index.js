ec974ce11b05f381adee4cc3ad09e18d0bbf8c06
yashovardhan212 <60869259+yashovardhan212@users.noreply.github.com>
2021-03-28 18:05:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -161,34 +161,6 @@ app.post("/db" , (req,res) => {
     const code = req.body.code;
     
     if(code == "1270011337"){
-        return res.send('{
-        "welcome.txt":"[+] W3lc0m3 t0 C1CADA",
-        "levels.txt":"There are total 7 levels in all.",
-        "about.txt":"This game is brought to you by students of IISERB.",
-        "contact.txt": "cicada.iiserb3301@gmail.com",
-        "dont_look_into_it.md":"I knew you will come for me! Wont find anything, lol!",
-        ".a-deen":"https://c1cada-iiserb.000webhostapp.com/ūnus/",
-        "no-des.txt":"Lullzzzzzz",
-        ".dva":"https://c1cada-iiserb.000webhostapp.com/duo/",
-        "ro0t.txt":"I am invisible.",
-        "look_if_you_aint_hurt.md":"I love f00ling you, hahaha!",
-        ".lev3l_1":"https://c1cada-iiserb.000webhostapp.com/level_1/",
-        "lev3l1.txt":"Ahh I so enjoy watching you search for the unknown.",
-        ".l3vel_2":"https://c1cada-iiserb.000webhostapp.com/level_2/",
-        "lev3l2.md":"Gotta agree, you wont stop until you find",
-        ".tree":"https://c1cada-iiserb.000webhostapp.com/trēs/",
-        ".chye-tir-ye":"https://c1cada-iiserb.000webhostapp.com/quattuor/",
-        "lev3l3.txt":"So desperate, huh!",
-        ".pyat":"https://c1cada-iiserb.000webhostapp.com/quīnque/",
-        "lev3l4.md":"Let's just be honest. You wont find it.",
-        ".shest":"https://c1cada-iiserb.000webhostapp.com/s3x/",
-        ".syem":"https://c1cada-iiserb.000webhostapp.com/septem/",
-        "lev3l5.txt":"How's the josh?? Could'nt find it, sir. Hahaha!!",
-        ".vo-syem":"https://c1cada-iiserb.000webhostapp.com/level_5/",
-        "lev3l6":"A free advice, get yourself an appointment from an Eye specialist. Probably he might help you LOOK for the right thing.",
-        ".dyev-yat":"https://c1cada-iiserb.000webhostapp.com/level_6/",
-        "lev3l7.txt":"Alright a free hint... Did you see it?",
-        ".dyes-yat":"https://c1cada-iiserb.000webhostapp.com/level_7/"
-    }')  }else{
+        return res.send("")  }else{
        return res.send("")}
 })
